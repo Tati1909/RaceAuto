71f323473c3c66a8bb6bbe94e0e94a5a513c9823
DESKTOP-9NLO4QL\emuar <emuart155@gmail.com>
2021-03-26 16:37:03 +0300

    Добавила проект из методички

---
+++ src/com/company/Race.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Race {
+    private ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+    }
+}
+
